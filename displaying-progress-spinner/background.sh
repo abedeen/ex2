d8df155d1f604615eb1c7a5f3cd54047be85ab23
Your Name <you@example.com>
2020-03-09 12:38:27 +0530

    New scenario 11

--- displaying-progress-spinner/background.sh
+++ displaying-progress-spinner/background.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "Hello..." >> /root/katacoda-finished
-sleep 2
-echo "Broken Auth " >> /root/katacoda-finished
-sleep 2
+sleep 5
 echo "done" >> /root/katacoda-finished
 sleep 5
 echo "done" >> /root/katacoda-background-finished
