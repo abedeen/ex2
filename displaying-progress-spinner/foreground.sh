28b62bf0f268b67fce024de7b53115ddc5240fea
Your Name <you@example.com>
2020-03-09 22:09:59 +0530

    New scenario 31

--- displaying-progress-spinner/foreground.sh
+++ displaying-progress-spinner/foreground.sh
@@ -1,2 +1,47 @@
+#!/bin/bash
+
+show_progress()
+{
+  echo -n "Starting1"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do 
+    sudo grep -i "done" /root/katacoda-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Starting"
+  echo -n "we are starting apisectm"
+  echo -n "http://104.42.41.159/swagger-ui.html "
+  while true; do 
+    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Configured"
+
+}
+
+
 sleep 1;echo "Hello..."  
-sleep 1; wait.sh
+sleep 1; show_progress
